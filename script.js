03557fd1f5384a4c4ae196c8f0359e9085feba2c
Maks <m.posiel@gmail.com>
2023-12-04 23:25:36 +0100

    Fixed svg size bug

--- script.js
+++ script.js
@@ -38,6 +38,8 @@ const displayController = (() => {
   const oSvgCode =
     '<svg style="fill: blue;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>circle</title><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>';
 
+  const nSvgCode =
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>blank</title></svg>';
   const updateBoard = (player) => {
     const tiles = document.querySelectorAll(".tile");
     tiles.forEach((tile, index) => {
@@ -45,6 +47,9 @@ const displayController = (() => {
         tile.innerHTML = xSvgCode;
       } else if (gameFlow.board.board[index] === "O") {
         tile.innerHTML = oSvgCode;
+      } else {
+        tile.innerHTML = nSvgCode;
+
       }
     });
 

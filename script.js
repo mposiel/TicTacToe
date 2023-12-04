bb437537b9aeb67441ef8a259b98c02389dcf84c
Maks <m.posiel@gmail.com>
2023-12-04 22:47:35 +0100

    Changed svg colors

--- script.js
+++ script.js
@@ -34,9 +34,9 @@ const infoManager = (() => {
 
 const displayController = (() => {
   const xSvgCode =
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>cross</title><path d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" /></svg>';
+    '<svg style="fill: red;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>cross</title><path d="M13.46,12L19,17.54V19H17.54L12,13.46L6.46,19H5V17.54L10.54,12L5,6.46V5H6.46L12,10.54L17.54,5H19V6.46L13.46,12Z" /></svg>';
   const oSvgCode =
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>circle</title><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>';
+    '<svg style="fill: blue;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>circle</title><path d="M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>';
 
   const updateBoard = (player) => {
     const tiles = document.querySelectorAll(".tile");
